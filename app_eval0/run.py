af4703e18a5f161a06ce919aea8608be0b507397
Aripatha <167372573+Aripatha@users.noreply.github.com>
2024-10-08 16:04:13 -0400

    Update run.py
    test update for run.py by aripat

--- app_eval0/run.py
+++ app_eval0/run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     print("Démarrage du serveur Flask...")
     app.run(debug=True)
-    print("Serveur démarré avec succès !")  # Cela ne sera pas exécuté tant que le serveur est en cours
+    print("Serveur démarré avec succès !")  # Cela ne sera pas exécuté tant que le serveur 
